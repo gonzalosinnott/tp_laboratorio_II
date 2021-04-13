989cae320253ecbc80e4997883c8698ea146e676
gonzalosinnott <gonzalo.sinnott@gmail.com>
2021-04-12 21:39:36 -0300

    GUI y esqueleto

--- tp1/MiCalculadora/Form1.Designer.cs
+++ tp1/MiCalculadora/Form1.Designer.cs
@@ -48,7 +48,6 @@ namespace MiCalculadora
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -104,7 +103,6 @@ namespace MiCalculadora
             this.label1.Size = new System.Drawing.Size(152, 55);
             this.label1.TabIndex = 8;
             this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
@@ -117,7 +115,7 @@ namespace MiCalculadora
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(254, 11);
+            this.textBox2.Location = new System.Drawing.Point(254, 13);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(115, 62);
             this.textBox2.TabIndex = 10;
@@ -140,7 +138,6 @@ namespace MiCalculadora
             this.MinimizeBox = false;
             this.Name = "FormCalculadora";
             this.Text = "Calculadora de Gonzalo Sinnott de 2° D";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- tp1/MiCalculadora/Form1.cs
+++ tp1/MiCalculadora/Form1.cs
@@ -17,19 +17,6 @@ namespace MiCalculadora
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
