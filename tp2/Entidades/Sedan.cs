8511db3cc22b02db62f46bfeddc72304d7b9b0b5
gonzalosinnott <gonzalo.sinnott@gmail.com>
2021-05-08 16:48:21 -0300

    "CLASE VEHICULO"

--- tp2/Entidades/Sedan.cs
+++ tp2/Entidades/Sedan.cs
@@ -10,7 +10,11 @@ namespace Entidades
 {
     class Sedan : Vehiculo
     {
-        public enum ETipo { CuatroPuertas, CincoPuertas }
+        public enum ETipo
+        {
+            CuatroPuertas, 
+            CincoPuertas
+        }
         ETipo tipo;
 
         /// <summary>
@@ -19,20 +23,24 @@ namespace Entidades
         /// <param name="marca"></param>
         /// <param name="chasis"></param>
         /// <param name="color"></param>
-        public Sedan(EMarca marca, string chasis, ConsoleColor color)
-            : base(chasis, marca, color)
+        public Sedan(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
         {
             tipo = ETipo.CuatroPuertas;
         }
 
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+           : this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
         /// <summary>
         /// Sedan son 'Mediano'
         /// </summary>
-        protected override short Tamanio
+        protected override ETamanio Tamanio
         {
             get
             {
-                return this.Tamanio;
+                return ETamanio.Mediano;
             }
         }
 
@@ -41,13 +49,13 @@ namespace Entidades
             StringBuilder sb = new StringBuilder();
 
             sb.AppendLine("SEDAN");
-            sb.AppendLine(this);
-            sb.AppendLine("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
             sb.AppendLine("TIPO : " + this.tipo);
             sb.AppendLine("");
             sb.AppendLine("---------------------");
 
-            return sb;
+            return sb.ToString();
         }
     }
 }
