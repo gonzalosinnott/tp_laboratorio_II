b3f71b9fac68706756ee3cbfa4c1d6b88a73a523
gonzalosinnott <gonzalo.sinnott@gmail.com>
2021-05-31 16:58:37 -0300

    Maquetado GUI

---
+++ tp3/GUI/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroSet_UI.Forms;
+using MaterialSkin.Animations;
+using MaterialSkin.Controls;
+
+
+namespace GUI
+{
+    public partial class frmMain : MaterialForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+}
